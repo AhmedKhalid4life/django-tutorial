c70a24ad82bd5525f7fed16670ea8c81749a23ed
Abdul <kingkhalid4life@gmail.com>
2024-09-30 20:45:32 +0100

    Assignment done

--- publish/serializers.py
+++ publish/serializers.py
@@ -6,12 +6,23 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model= Author
         fields= "__all__"
+
+    
     
 class BookSerializer(serializers.ModelSerializer):
     author= AuthorSerializer()
     class Meta:
         model= Books
         fields= "__all__"
+    
+    #create
+    def create(self, validated_data):
+        author_data = validated_data.pop('author')
+        author,_ = Author.objects.get_or_create(**author_data)
+        book = Books.objects.create(author= author, **validated_data)
+        return book
+    
+
 
 class ReviewSerializers(serializers.ModelSerializer):
     book= BookSerializer()
@@ -19,3 +30,10 @@ class ReviewSerializers(serializers.ModelSerializer):
     class Meta:
         model= Review
         fields= "__all__"
+
+    
+    def create(self, validated_data):
+        books_data = validated_data.pop('book')
+        book,_ = Books.objects.get_or_create(**books_data)
+        book = Review.objects.create(book = book, **validated_data)
+        return book
--- publish/urls.py
+++ publish/urls.py
@@ -5,7 +5,16 @@ from .views import (
     ReviewListView,
     AuthorsWithMultipleBooksView,
     BookWithReviewsView,
-    TopAuthorsView
+    TopAuthorsView,
+    AuthorCreateView,
+    BookCreateView,
+    ReviewCreateView,
+    AuthorDeleteView,
+    BookDeleteView,
+    ReviewDeleteView,
+    AuthorUpdateView,
+    BookUpdateView,
+    ReviewUpdateView
 )
 
 urlpatterns = [
@@ -15,4 +24,20 @@ urlpatterns = [
     path('authors/multiple-books/', AuthorsWithMultipleBooksView.as_view(), name='authors-multiple-books'),
     path('books/with-reviews/', BookWithReviewsView.as_view(), name='books-with-reviews'),
     path('authors/top/', TopAuthorsView.as_view(), name='top-authors'),
+
+    #create
+    path('author/create/',AuthorCreateView.as_view(), name = "author-create"),
+    path('book/create/',BookCreateView.as_view(), name = "book-create"),
+    path('review/create/',ReviewCreateView.as_view(), name = "review-create"),
+    
+    #delete
+    path('author/<int:pk>/delete/',AuthorDeleteView.as_view(), name = "author-delete"),
+    path('book/<int:pk>/delete/',BookDeleteView.as_view(), name = "book-delete"),
+    path('review/<int:pk>/delete/',ReviewDeleteView.as_view(), name = "review-delete"),
+
+    #update
+    path('author/<int:pk>/update/',AuthorUpdateView.as_view(), name = "author-update"),
+    path('book/<int:pk>/update/',BookUpdateView.as_view(), name = "book-update"),
+    path('review/<int:pk>/update/',ReviewUpdateView.as_view(), name = "review-update"),
+
 ]
